c2e2289608944690969826aca1f48f01954b605a
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-01-17 19:08:43 +0530

    adding files for spring boot

--- DemoApplication.java
+++ DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,3 +11,12 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from AWS CodeBuild with Spring Boot!";
+    }
+}
+
